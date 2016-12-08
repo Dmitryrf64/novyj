8870c20a218d0a9f197467c9834e590863596fe5
Dmitryrf64 <79370253350@yandex.ru>
2016-12-08 12:35:10 +0300

    Update index.php

--- index.php
+++ index.php
@@ -59,20 +59,3 @@ catch (PDOException $e) {
 ?>
 </body>
 </html>
-Внутри тегов PHP добавьте код PHP для подключения к базе данных.
-
-// DB connection info
-$host = "localhost\sqlexpress";
-$user = "user name";
-$pwd = "password";
-$db = "registration";
-// Connect to database.
-try {
-    $conn = new PDO
-( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
-    $conn->setAttribute
-( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-}
-catch(Exception $e){
-    die(var_dump($e));
-}
