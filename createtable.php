2811778c3fdda0f5395995842019b001a2600adb
Dmitryrf64 <79370253350@yandex.ru>
2016-12-12 09:46:25 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -6,7 +6,7 @@ $pwd = "password";
 $db = "registration";
 try{
     $conn = new PDO
-( "sqlsrv:server = tcp:registration123.database.windows.net,1433; Database = Base22", "аdmon", "{Parfenov25}");
+( "sqlsrv:server = tcp:registration123.database.windows.net,1433; Database = "Base22", "аdmon", "{Parfenov25}");
     $conn->setAttribute
 ( PDO::ATTR_ERRMODE, 
 PDO::ERRMODE_EXCEPTION );
